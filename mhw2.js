68461706644546564bfaa7288aba1284f5105bd8
Francesco Mirenda <61842200+shadowlinkct@users.noreply.github.com>
2024-03-30 19:52:27 +0100

    Add files via upload

--- mhw2.js
+++ mhw2.js
@@ -14,3 +14,30 @@ function topbarclose() {
 
 const menubutt = document.getElementById('menu');
 menubutt.addEventListener('click', topbar);
+
+// Seleziona tutti i div principali
+const mainDivs = document.querySelectorAll('.centerlink div');
+
+// Aggiungi un listener di evento a ciascun div principale
+mainDivs.forEach(div => {
+    // Aggiungi un listener di evento per il click a ciascun div principale
+    div.addEventListener('click', function() {
+        // Ottieni l'id del dropdown content corrispondente
+        const contentId = this.id + 'Content';
+        const content = document.getElementById(contentId);
+
+        // Verifica se il dropdown content è già aperto
+        const isOpen = content.classList.contains('active');
+
+        // Rimuovi la classe 'active' da tutti i dropdown content
+        document.querySelectorAll('.dropdown-content.active')
+            .forEach(activeContent => activeContent.classList.remove('active'));
+
+        // Se il dropdown content è già aperto, non fare nient'altro (ovvero, chiudilo)
+        if (!isOpen) {
+            // Aggiungi la classe 'active' solo al dropdown content corrispondente
+            content.classList.add('active');
+        }
+    });
+});
+
