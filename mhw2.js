0bc16328b02cb057511374737dd64146d4164d0a
Francesco Mirenda <61842200+shadowlinkct@users.noreply.github.com>
2024-04-04 21:05:55 +0200

    Add files via upload

--- mhw2.js
+++ mhw2.js
@@ -1,15 +1,15 @@
 function topbar() {
     const activeDiv = document.getElementById('active');
     activeDiv.classList.remove('hidden');
-    menubutt.removeEventListener('click', topbar); // Rimuovi l'event listener per topbar
-    menubutt.addEventListener('click', topbarclose); // Aggiungi l'event listener per topbarclose
+    menubutt.removeEventListener('click', topbar);
+    menubutt.addEventListener('click', topbarclose);
 }
 
 function topbarclose() {
     const activeDiv = document.getElementById('active');
     activeDiv.classList.add('hidden');
-    menubutt.removeEventListener('click', topbarclose); // Rimuovi l'event listener per topbarclose
-    menubutt.addEventListener('click', topbar); // Aggiungi l'event listener per topbar
+    menubutt.removeEventListener('click', topbarclose);
+    menubutt.addEventListener('click', topbar);
 }
 
 const menubutt = document.getElementById('menu');
@@ -20,27 +20,47 @@ const RIGHT_ARROW = 'img/forward-arrow.png';
 const DOWN_ARROW = 'img/down-arrow.png';
 
 function toggle(event) {
-  const targetId = event.currentTarget.id + "Content"; // Ottieni l'ID del contenuto correlato
-  const content = document.getElementById(targetId);
-  
-  isVisible = !isVisible;
-  if (isVisible) {
-      content.classList.remove('hidden');
-      event.currentTarget.querySelector('img').src = DOWN_ARROW;
-  } else {
-      content.classList.add('hidden');
-      event.currentTarget.querySelector('img').src = RIGHT_ARROW;
-  }
+    const targetId = event.currentTarget.id + "Content";
+    const content = document.getElementById(targetId);
+
+    isVisible = !isVisible;
+    if (isVisible) {
+        content.classList.remove('hidden');
+        event.currentTarget.querySelector('img').src = DOWN_ARROW;
+    } else {
+        content.classList.add('hidden');
+        event.currentTarget.querySelector('img').src = RIGHT_ARROW;
+    }
 }
 
 let isVisible = false;
-function handleClickOutside(event) {
-  isVisible = false;
-  // Esegui altre azioni necessarie quando viene fatto clic al di fuori degli elementi toggle
+function clickesterno(event) {
+    // Verifica se l'elemento cliccato non è uno dei toggleItems
+    let esternocliccato = true;
+    for (let i = 0; i < toggleItems.length; i++) {
+        if (toggleItems[i].contains(event.target)) {
+            esternocliccato = false;
+            break;
+        }
+        console.log("esternocliccato:", esternocliccato);
+    }
+
+    // Se il click è esterno e isVisible è true, nascondi tutti gli elementi
+    if (esternocliccato && isVisible) {
+        for (let i = 0; i < toggleItems.length; i++) {
+            const targetId = toggleItems[i].id + "Content";
+            const content = document.getElementById(targetId);
+            content.classList.add('hidden');
+            toggleItems[i].querySelector('img').src = RIGHT_ARROW;
+        }
+        isVisible = false;
+        console.log("isVisible:", isVisible);
+    }
 }
-const toggleItems = document.querySelectorAll('.toggle-item');
+
+const toggleItems = document.querySelectorAll('.centerlink-item');
 
 for (let i = 0; i < toggleItems.length; i++) {
-  toggleItems[i].addEventListener('click', toggle);
+    toggleItems[i].addEventListener('click', toggle);
 }
-
+document.addEventListener("click", clickesterno);
