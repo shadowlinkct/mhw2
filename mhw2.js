20b8acff0cff5e13cff8ac154e149f0e641dcea1
Francesco Mirenda <61842200+shadowlinkct@users.noreply.github.com>
2024-04-05 10:11:05 +0200

    Add files via upload

--- mhw2.js
+++ mhw2.js
@@ -1,10 +1,11 @@
+
 const RIGHT_ARROW = 'img/forward-arrow.png';
 const DOWN_ARROW = 'img/down-arrow.png';
 
 
 function toggleTopbar() {
-  const activeDiv = document.getElementById('active');
-  activeDiv.classList.toggle('hidden');
+    const activeDiv = document.getElementById('active');
+    activeDiv.classList.toggle('hidden');
 }
 
 const menubutt = document.getElementById('menu');
@@ -28,66 +29,66 @@ menubutt.addEventListener('click', toggleTopbar);
 // const menubutt = document.getElementById('menu');
 // menubutt.addEventListener('click', topbar);
 
-function toggle(event) {
-  const targetId = event.currentTarget.id + "Content";
-  const content = document.getElementById(targetId);
-
-  isVisible = !isVisible;
-  if (isVisible) {
-    content.classList.remove('hidden');
-    event.currentTarget.querySelector('img').src = DOWN_ARROW;
-  } else {
-    content.classList.add('hidden');
-    event.currentTarget.querySelector('img').src = RIGHT_ARROW;
-  }
-}
-
+//CENTERLINKFINESTRE
 let isVisible = false;
-function clickesterno(event) {
-  // Verifica se l'elemento cliccato non è uno dei toggleItems
-  let esternocliccato = true;
-  for (let i = 0; i < toggleItems.length; i++) {
-    if (toggleItems[i].contains(event.target)) {
-      esternocliccato = false;
-      break;
-    }
-    console.log("esternocliccato:", esternocliccato);
-  }
+function toggle(event) {
+    const targetId = event.currentTarget.id + "Content";
+    const content = document.getElementById(targetId);
 
-  // Se il click è esterno e isVisible è true, nascondi tutti gli elementi
-  if (esternocliccato && isVisible) {
-    for (let i = 0; i < toggleItems.length; i++) {
-      const targetId = toggleItems[i].id + "Content";
-      const content = document.getElementById(targetId);
-      content.classList.add('hidden');
-      toggleItems[i].querySelector('img').src = RIGHT_ARROW;
+    isVisible = !isVisible;
+    if (isVisible) {
+        content.classList.remove('hidden');
+        event.currentTarget.querySelector('img').src = DOWN_ARROW;
+    } else {
+        content.classList.add('hidden');
+        event.currentTarget.querySelector('img').src = RIGHT_ARROW;
     }
-    isVisible = false;
-    console.log("isVisible:", isVisible);
-  }
+}
+function clickesterno(event) {
+    // Verifica se l'elemento cliccato non è uno dei toggleItems
+    let esternocliccato = true;
+    for (let i = 0; i < toggleItems.length; i++) {
+        if (toggleItems[i].contains(event.target)) {
+            esternocliccato = false;
+            break;
+        }
+        console.log("esternocliccato:", esternocliccato);
+    }
+
+    // Se il click è esterno e isVisible è true, nascondi tutti gli elementi
+    if (esternocliccato && isVisible) {
+        for (let i = 0; i < toggleItems.length; i++) {
+            const targetId = toggleItems[i].id + "Content";
+            const content = document.getElementById(targetId);
+            content.classList.add('hidden');
+            toggleItems[i].querySelector('img').src = RIGHT_ARROW;
+        }
+        isVisible = false;
+        console.log("isVisible:", isVisible);
+    }
 }
 
 const toggleItems = document.querySelectorAll('.centerlink-item');
 
 for (let i = 0; i < toggleItems.length; i++) {
-  toggleItems[i].addEventListener('click', toggle);
+    toggleItems[i].addEventListener('click', toggle);
 }
 document.addEventListener("click", clickesterno);
 
-
-function changeHeartImage(heart) {
-  heart.classList.toggle('clicked');
-  if (heart.classList.contains('clicked')) {
-      heart.src = 'img/heart.png';
-  } else {
-      heart.src = 'img/heartno.png';
-  }
+//ICONAPREFERITI
+function changebookmarkImage(bookmark) {
+    bookmark.classList.toggle('clicked');
+    if (bookmark.classList.contains('clicked')) {
+        bookmark.src = 'img/bookmark.png';
+    } else {
+        bookmark.src = 'img/bookmarkno.png';
+    }
 }
 
-for (let i = 1; i <= 1; i++) {
-  const heart = document.getElementById('heart' + i);
-  function handleClick() {
-    changeHeartImage(this);
-}
-heart.addEventListener('click', handleClick);
+for (let i = 1; i <= 14; i++) {
+    const bookmark = document.getElementById('bookmark' + i);
+    function handleClick() {
+        changebookmarkImage(this);
+    }
+    bookmark.addEventListener('click', handleClick);
 }
